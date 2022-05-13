518b4bfbe9af5f6eca94200b8530f86af46d3386
Eps1lon117 <83192784+Eps1lon117@users.noreply.github.com>
2022-05-12 22:31:48 -0500

    Update ESTUDIANTE.py

--- ESTUDIANTE.py
+++ ESTUDIANTE.py
@@ -1,15 +1,18 @@
+"""Una de las clases principales para el funcionamiento del programa, se definen atributos principales y para el guardado
+en la base de datos se usa como PK el ID o cedula, támbien en un futuro se usará para hacecr login, solo se definen setters
+y getters ya que la función principal de esta clase es capturar los datos del usuario para introducirlos a la BD"""
 
 class Estudiante:
     def __int__(self):
         self.ID = None
         self.F_Name = None
         self.L_Name = None
+        self.Edad = None
         self.E_mail = None
         self.City = None
-        self.Gamer = 0
-        self.Sport = 0
-        self.Series = 0
-        self.Study = 0
+        self.Gamer = None
+        self.Series_tv = None
+
 
     def get_id(self):
         return self.ID
@@ -20,6 +23,9 @@ class Estudiante:
     def get_l_name(self):
         return self.L_Name
 
+    def get_edad(self):
+        return self.Edad
+
     def get_e_mail(self):
         return self.E_mail
 
@@ -29,14 +35,8 @@ class Estudiante:
     def get_gamer(self):
         return self.Gamer
 
-    def get_sport(self):
-        return self.Sport
-
     def get_series(self):
-        return self.Series
-
-    def get_study(self):
-        return self.Study
+        return self.Series_tv
 
     def set_id(self):
         self.ID = input('Identificación: ')
@@ -48,6 +48,9 @@ class Estudiante:
     def set_l_name(self):
         self.L_Name = input('Apellidos: ')
 
+    def set_edad(self):
+        self.Edad = int(input('Edad: '))
+
     def set_e_mail(self):
         self.E_mail = input('Correo Institucional: ')
 
@@ -57,40 +60,15 @@ class Estudiante:
     def set_gamer(self):
         self.Gamer = int(input('¿Te gustan los videojuegos?(1 para si 0 para no): '))
 
-    def set_sport(self):
-        self.Sport = int(input('¿Te gustan los Deportes?(1 para si 0 para no): '))
-
     def set_series(self):
-        self.Series = int(input('¿Te gustan las Series?(1 para si 0 para no): )'))
+        self.Series_tv = int(input('¿Te gustan las Series?(1 para si 0 para no): )'))
 
-    def set_study(self):
-        self.Study = int(input('¿Buscas con quien estudiar?(1 para si 0 para no): '))
-
-# el metodo datos ayuda a recopilar la información del objeto estudiante para multiples usos, desde registro
-# hasta busqueda, este metodo retorma una tupla de strings y enteros para ingresar a la BD o consultar.
-
+"""el metodo datos ayuda a recopilar la información del objeto estudiante para multiples usos, desde registro
+hasta busqueda, este metodo retorma una tupla de strings y enteros para ingresar a la BD o consultar."""
+    
     def datos(self):
-        datos = (self.ID, self.F_Name, self.L_Name, self.E_mail, self.City, self.Gamer, self.Sport, self.Series, self.Study)
+        datos = (self.ID, self.F_Name, self.L_Name, self.Edad, self.E_mail, self.City, self.Gamer, self.Series_tv)
         return datos
-
-# metodo para ingresar los datos del usuario en la tabla de usuarios principal, usa sentecias sql
-# hay que tener en cuenta que depues del insert into se debe colocar el nombre de la tabla de destino,
-# y los interrogantes es la cantidad de valores que pide la tabla y la ultima variable es una tupla de string y enteros
-# con los datos a ingresar en la tabla esta última es muy importante
-
-    def ingresar_usuario(self, con, d_usuario):
-        cursor_obj = con.cursor()
-        cursor_obj.execute('''INSERT INTO Usuarios VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', d_usuario)
-        con.commit()
-        return True
-
-# lase gamers con herencia de estudiante para poder usar el identificador de registro del usuario
-    def consulta_general(self, con):
-        cursor_obj = con.cursor()
-        consulta = "SELECT * FROM Usuarios"
-        cursor_obj.execute(consulta)
-        filas = cursor_obj.fetchall()
-        for row in filas:
-            codigo_plan = row[0]
-            cedula_cleinte = row[1]
-            print(row)
+    
+    
+    
